170cd919f66a4fc4dd2cdc9caeb001539ca23e27
Web development Thousand <back@1-000.kz>
2020-03-11 12:34:31 +0600

    First

---
+++ src/app/Http/Controllers/ThumbController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Thumb;
+use Illuminate\Http\Request;
+
+class ThumbController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Thumb  $thumb
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Thumb $thumb)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Thumb  $thumb
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Thumb $thumb)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Thumb  $thumb
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Thumb $thumb)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Thumb  $thumb
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Thumb $thumb)
+    {
+        //
+    }
+}
